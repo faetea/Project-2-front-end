8e15c8358bf4ab0b46db7c92ef0039fd28dfebd4
saphron <saphron.scul@gmail.com>
2015-11-09 21:01:14 -0500

    Adding Register and Login to Index

---
+++ assets/js/project.js
@@ -0,0 +1,13 @@
+$('#btn').click(function() {
+    $('#wizard').toggle();
+});
+
+// <script type="text/javascript">
+//    function toggle_visibility(id) {
+//        var e = document.getElementById(id);
+//        if(e.style.display == 'block')
+//           e.style.display = 'none';
+//        else
+//           e.style.display = 'block';
+//    }
+// </script>
