ba2001516f73c2911c90642fdd0943911461c1d9
saphron <saphron.scul@gmail.com>
2015-11-10 00:04:51 -0500

    connected front-end to back-end api with ajax calls added button id's to index.html and link to project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,13 +1,52 @@
-$('#btn').click(function() {
-    $('#wizard').toggle();
+var baseapi = {
+  base: 'http://localhost:3000',
+
+  ajax: function(config, cb) {
+    $.ajax(config).done(function(data, textStatus, jqxhr) {
+      cb(null, data);
+    }).fail(function(jqxhr, status, error) {
+      cb({jqxhr: jqxhr, status: status, error: error});
+    });
+  },
+
+  register: function register(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.base + '/register',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    }, callback);
+  },
+
+  login: function login(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.base + '/login',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    }, callback);
+  },
+
+}
+
+$(document).ready(function(){
+    $("#register").click(function(){
+      var credentials = {
+        "credentials": {
+          "email": $("#inputEmail").val(),
+          "password": $("#inputPassword").val(),
+          "password_confirmation": $("#inputPassword").val()
+        }
+      };
+
+      baseapi.register(credentials, null);
+    });
+    // $("#login").click(function(){
+    //   };
+
+    //   baseapi.login(credentials, callback);
+    // });
 });
 
-// <script type="text/javascript">
-//    function toggle_visibility(id) {
-//        var e = document.getElementById(id);
-//        if(e.style.display == 'block')
-//           e.style.display = 'none';
-//        else
-//           e.style.display = 'block';
-//    }
-// </script>
